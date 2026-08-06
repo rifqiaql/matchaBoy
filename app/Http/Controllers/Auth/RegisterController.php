0621ede0fc5bebfc1716116f097a9412d24bda9b
Raihan <rhanz1909@gmail.com>
2026-08-06 19:50:11 +0700

    testing komentar

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -35,6 +35,7 @@ class RegisterController extends Controller
             'password' => Hash::make($validated['password']),
             'role' => 'karyawan', // GEMBOK KEAMANAN: Memaksa pendaftar publik menjadi Karyawan
         ]);
+        // dicek ulang terkait role yang di hardcode, jika ingin role lain bisa diubah disini
 
         // 3. Autentikasi dan Arahkan ke Dashboard
         Auth::login($user);
